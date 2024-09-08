a4374ae27acefe732cff05ffd2d3b875a30bc36d
Swastik <swastikiscoding@gmail.com>
2024-09-08 17:25:47 +0530

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'prism': ["Tilt Prism", "sans-serif"],
+        'protest': ["Protest Guerrilla", "sans-serif"],
+        'suse': ["SUSE", "sans-serif"],
+        'handjet': ["Handjet", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
